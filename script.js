3f8bb7d46d39c047df1920077ae2c521c97f5552
tenesio <39808312+LuanRebuli@users.noreply.github.com>
2025-11-11 11:01:42 -0300

    teste aula11/11

---
+++ script.js
@@ -0,0 +1,22 @@
+const nome = "nome";
+let idade = 24;
+var cpf = 1223123123123;
+/* console.log("DADOS DO USUARIO \n");
+console.log("Nome ", nome,"\n");
+console.log("Idade ", idade, "\n")
+console.log("CPF ", cpf,"\n");
+console.log("-------------------") */
+
+function adicionar(){
+    idade++;
+    console.log(idade);
+}
+
+const typed = new Typed(".multiple-text",{
+    strings:[
+    
+    "leite",
+    "leite2",
+    ],
+    typespeed : 100,
+})
